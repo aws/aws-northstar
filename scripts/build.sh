3b6334e7eb5f388c3abef44b1fb89ea4d84827b5
Jessie Wei <jessieweiyi@gmail.com>
2020-10-27 18:26:25 +1100

    build: Setup Travis CI Build (#6)

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,14 +2,18 @@
 
 # Build script is to be run from the root directory of the aws-northstar package
 
-set -e
+set -ev
 
-npm install
+npm ci
 npm run test:all
 npm run generate:attribution
 npm run styleguide:build
 npm run storybook:build
-if [ -d "./build" ]; then rm -rf ./build; fi # make sure the npm package only includes clean built components
+
+echo 'Remove build folder to make sure the npm package only includes clean built components'
+if [ -d "./build" ]; then rm -rf ./build; fi # 
 npm run build
+
+echo 'Copy the examples to published examples folder'
 if [ ! -d "./styleguide.out/examples" ]; then mkdir -p styleguide.out/examples ; fi
 cd examples && tar -czvf ../styleguide.out/examples/create-react-app.tar.gz ./create-react-app && cd -
