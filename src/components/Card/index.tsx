f3ea36e73cda1b755fba200332e9ac02ad7f2faf
Jessie Wei <jessieweiyi@gmail.com>
2020-10-30 13:14:05 +1100

    fix(Card): Add mouse events to the card (#25)
    Co-authored-by: Jessie Wei <jeswei@amazon.com>

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -14,7 +14,7 @@
   limitations under the License.                                                                              *
  ******************************************************************************************************************** */
 
-import React, { FunctionComponent, ReactNode } from 'react';
+import React, { FunctionComponent, ReactNode, MouseEvent } from 'react';
 import MUICard from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardHeader from '@material-ui/core/CardHeader';
@@ -33,19 +33,43 @@ export interface CardProps {
      * Content of the card
      * */
     children?: ReactNode;
+    /**
+     * Fired when the user clicks on the card.
+     */
+    onClick?: (event: MouseEvent<HTMLDivElement>) => void;
+    /**
+     * Fired when the mouse pointer is moved onto the card, or onto one of its children.
+     */
+    onMouseEnter?: (event: MouseEvent<HTMLDivElement>) => void;
+    /**
+     * Fired when moving the mouse pointer onto the card.
+     */
+    onMouseMove?: (event: MouseEvent<HTMLDivElement>) => void;
+    /**
+     * Fired when moving the mouse pointer out of the card.
+     */
+    onMouseOut?: (event: MouseEvent<HTMLDivElement>) => void;
 }
 
 /**
  * A Card renders the basic information of a resource
  * */
-const Card: FunctionComponent<CardProps> = ({ title = '', subtitle = '', children }) => {
+const Card: FunctionComponent<CardProps> = ({
+    title = '',
+    subtitle = '',
+    children,
+    onClick,
+    onMouseEnter,
+    onMouseMove,
+    onMouseOut,
+}) => {
     const content = (
         <CardContent>
             {typeof children === 'string' ? <Text variant="p">{children}</Text> : <>{children}</>}
         </CardContent>
     );
     return (
-        <MUICard>
+        <MUICard onClick={onClick} onMouseEnter={onMouseEnter} onMouseMove={onMouseMove} onMouseOut={onMouseOut}>
             <CardHeader title={title} subheader={subtitle} />
             {content}
         </MUICard>
