01227ecc97f185285ac999534a88e9a0645d274f
Jack Stevenson <jacsteve@amazon.com>
2020-10-30 15:02:36 +1100

    fix: remove unused imports

--- src/components/BreadcrumbGroup/index.test.tsx
+++ src/components/BreadcrumbGroup/index.test.tsx
@@ -16,7 +16,7 @@
 
 import * as React from 'react';
 import { MemoryRouter } from 'react-router';
-import { render, getByText, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import BreadcrumbGroup from '.';
 
 describe('BreadcrumbGroup', () => {
